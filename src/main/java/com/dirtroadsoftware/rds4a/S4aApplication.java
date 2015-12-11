df65f1a6d1814ce634112ec22255b3e4a5a3c094
antonevane <antonevane@gmail.com>
2015-12-11 07:23:33 -0500

    Introduced basic spring boot configuration

---
+++ src/main/java/com/dirtroadsoftware/rds4a/S4aApplication.java
@@ -0,0 +1,12 @@
+package com.dirtroadsoftware.rds4a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S4aApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(S4aApplication.class, args);
+    }
+}
