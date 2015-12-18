fedc26260a78af9a2fc59d441adbd0e873b90ec6
Anton Novopashin <antonevane@gmail.com>
2015-12-18 14:00:33 -0500

    Introduces Spring Security java config class. (To be implemented)

---
+++ src/main/java/com/dirtroadsoftware/rds4a/configuration/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.dirtroadsoftware.rds4a.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+// TODO: Will be refined later
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+}
