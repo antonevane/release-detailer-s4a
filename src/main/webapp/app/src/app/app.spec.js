cb7051e3797b6c167da5ef115fb037f9b3ac568c
antonevane <antonevane@gmail.com>
2016-01-23 05:31:54 -0500

    /**

--- src/main/webapp/app/src/app/app.spec.js
+++ src/main/webapp/app/src/app/app.spec.js
@@ -1,3 +1,9 @@
+/**
+ * Tests sit right alongside the file they are testing, which is more intuitive
+ * and portable than separating `src` and `test` directories. Additionally, the
+ * build process will exclude all `.spec.js` files from the build
+ * automatically.
+ */
 describe( 'AppCtrl', function() {
   describe( 'isCurrentUrl', function() {
     var AppCtrl, $location, $scope;
